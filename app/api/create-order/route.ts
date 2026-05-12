b9f22c2d1e1d49f5755327a04cac4c412fd2b378
shahroznasir <mdshahroznasir@gmail.com>
2026-05-12 11:24:43 +0530

    ready for deployment

--- app/api/create-order/route.ts
+++ app/api/create-order/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   try {
     const razorpay = new Razorpay({
-      key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
+      key_id: process.env.RAZORPAY_KEY_ID!,
       key_secret: process.env.RAZORPAY_KEY_SECRET!,
     });
 
